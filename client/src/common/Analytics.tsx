553f30f69798af0b2a7c90bc9c18dab4ebfafba0
Jeffrey Maas <Jmaasy@users.noreply.github.com>
2025-07-23 00:37:52 +0200

    update track code

--- client/src/common/Analytics.tsx
+++ client/src/common/Analytics.tsx
@@ -2,52 +2,57 @@ import ReactGA from 'react-ga4';
 import { UaEventOptions } from 'react-ga4/types/ga4';
 
 const sendEvent = (args: UaEventOptions) => ReactGA.event(args);
+const trackEvent = (event: string) => {
+    if (umami != undefined) {
+        umami.track(event);
+    }
+}
 
 export const Analytics = {
     trackVotesReveal: (lobbyId: string) => {
         const args = {category: "VotesReveal", action: "revealing_votes", label: `RevealingVotes${lobbyId}`};
         sendEvent(args);
-        umami.track('votes-revealed');
+        trackEvent('votes-revealed');
     },
     trackVoting: (vote: number | string) => {
         const args = {category: "VoteUpdate", action: "updating_vote", label: vote.toString()};
         sendEvent(args);
-        umami.track(`vote-${vote}`);
+        trackEvent(`vote-${vote}`);
     },
     trackRegistering: () => {
         const args = {category: "UserRegistered", action: "user_registered", label: `User Registered`};
         sendEvent(args);
-        umami.track('user-registered');
+        trackEvent('user-registered');
     },
     trackCreatedLobby: () => {
         const args = {category: "UserCreatedLobby", action: "user_created_lobby", label: `User Created A Lobby`};
         sendEvent(args);
-        umami.track('user-created-lobby');
+        trackEvent('user-created-lobby');
     },
     trackJoinedLobby: () => {
         const args = {category: "UserJoinedLobby", action: "user_joined_lobby", label: `User Joined A Lobby`};
         sendEvent(args);
-        umami.track('user-joined-lobby');
+        trackEvent('user-joined-lobby');
     },
     trackLeftLobby: () => {
         const args = {category: "UserLeftLobby", action: "user_left_lobby", label: `User Left A Lobby`};
         sendEvent(args);
-        umami.track('user-left-lobby');
+        trackEvent('user-left-lobby');
     },
     trackConnectionLost: () => {
         const args = {category: "ConnectionLost", action: "connection_lost", label: `Connection to server was lost`};
         sendEvent(args);
-        umami.track('connection-lost');
+        trackEvent('connection-lost');
     },
     trackConnectionSuccess: () => {
         const args = {category: "ConnectionSuccess", action: "connection_success", label: `Connection to server was successful`};
         sendEvent(args);
-        umami.track('connection-success');
+        trackEvent('connection-success');
     },
     trackConnectionRegained: () => {
         const args = {category: "ConnectionRegained", action: "connection_regained", label: `Connection to server has restored`};
         sendEvent(args);
-        umami.track('connection-regained');
+        trackEvent('connection-regained');
     },
     initializeTracking: () => {
         ReactGA.initialize("G-CCVCFQYZ2F");
